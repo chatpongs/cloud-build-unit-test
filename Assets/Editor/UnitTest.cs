4cadbffe212da8414e3f9232b034fe30a8fa23ee
Chatpong Suteesuksatapon <chatpong@progaming.co.th>
2017-07-02 22:44:26 +0700

    initial commit

---
+++ Assets/Editor/UnitTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class UnitTest : MonoBehaviour {
+
+	[Test]
+	public static void AlwaysPass() 
+    {
+        Assert.That(true);
+	}
+}
