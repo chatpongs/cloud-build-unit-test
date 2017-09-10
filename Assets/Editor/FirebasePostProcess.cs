00748d6bba168059f3cf841a848022a5cd65c5e9
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2017-09-11 06:53:17 +0700

    disable bitcode

---
+++ Assets/Editor/FirebasePostProcess.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR && UNITY_IOS
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public static class FirebasePostProcess {
+	[PostProcessBuild]
+	private static void ProcessPostBuild (BuildTarget buildTarget, string path)
+	{
+		// Only perform these steps for iOS builds
+		#if UNITY_IOS
+		string projPath = PBXProject.GetPBXProjectPath(path);
+		PBXProject project = new PBXProject();
+		project.ReadFromString(File.ReadAllText(projPath));
+		string target = project.TargetGuidByName(PBXProject.GetUnityTargetName());
+		
+		project.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+		
+		File.WriteAllText(projPath, project.WriteToString());
+		#endif
+	}
+}
+#endif
